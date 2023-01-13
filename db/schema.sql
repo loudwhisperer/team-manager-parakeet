35c6f22b24bcd81758e6ea295b6934e6e8242ee9
loudwhisperer <driftaway44@gmail.com>
2023-01-12 21:37:38 -0700

    added schema and seeds content to files and set up remaining file structure

--- db/schema.sql
+++ db/schema.sql
@@ -1,36 +1,32 @@
-DROP DATABASE IF EXISTS organizer_db
-CREATE DATABASE organizer_db
-
-USE organizer_db
+DROP DATABASE IF EXISTS organizer_db;
+CREATE DATABASE organizer_db;
+USE organizer_db;
 
 CREATE TABLE department (
-    id INT NOT NULL, 
-    name VARCHAR(30) NOT NULL,
-    PRIMARY KEY (id)
+    id INT PRIMARY KEY AUTO_INCREMENT, 
+    name VARCHAR(30) NOT NULL
 );
 
 CREATE TABLE role (
-    id INT NOT NULL, 
+    id INT PRIMARY KEY AUTO_INCREMENT, 
     title VARCHAR(30) NOT NULL,
-    salary DECIMAL NOT NULL,
-    department_id INT NOT NULL,
-    PRIMARY KEY (id)
+    salary DECIMAL(10, 2) NOT NULL,
+    department_id INT,
     FOREIGN KEY (department_id)
-  REFERENCES department(id)
-  ON DELETE SET NULL
+    REFERENCES department(id)
+    ON DELETE SET NULL
 );
 
 CREATE TABLE employee (
-    id INT NOT NULL,
+    id INT PRIMARY KEY AUTO_INCREMENT, 
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
-    role_id INT NOT NULL,
-    manager_id INT NOT NULL,
-  FOREIGN KEY (manager_id)
-  REFERENCES manager(id)
-  ON DELETE SET NULL
-     FOREIGN KEY (role_id)
-  REFERENCES role(id)
-  ON DELETE SET NULL
+    role_id INT,
+    manager_id INT,
+    FOREIGN KEY (manager_id)
+    REFERENCES employee(id)
+    FOREIGN KEY (role_id)
+    REFERENCES role(id)
+    ON DELETE SET NULL
 );
 
---
+++ db/seeds.sql
@@ -0,0 +1,20 @@
+INSERT INTO department (name)
+
+VALUES
+('IT'),
+('development'),
+('finance');
+
+INSERT INTO role (title, salary, department_id)
+
+VALUES
+('Admin', 98,000, 1)
+('engineer', 125,000, 2)
+('money-person', 75,000, 3)
+
+INSERT INTO role (first_name, last_name, role_id, manager_id)
+
+VALUES
+('David', 'Man', 1, 2)
+('Lesley', 'Humanson', 2, 1)
+('Big Bucks', 'Johnson', 3, 1)
