83421fa128e5075a41015d17bd1a13341a83e63b
loudwhisperer <driftaway44@gmail.com>
2023-01-20 07:56:46 -0700

    pushing to troubleshoot possible env issues

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,11 @@
 const mysql = require('mysql2');
+const dotenv = require('dotenv');
 const db = mysql.createConnection(
   process.env.DB_NAME,
   process.env.DB_USER,
   process.env.DB_PASSWORD,
   {
     host: 'localhost',
-    dialect: 'mysql',
   },
   console.log(`Connected to the organizer_db database.`)
 );
